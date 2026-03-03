dc82fde58467fed5e6c620a752d26aa5739b15b5
Gionaldo Chandrawansah <122026114+GionaldoCandr@users.noreply.github.com>
2026-03-03 14:43:13 +0700

    Update pretest_m2.java

--- src/pretest_m2.java
+++ src/pretest_m2.java
@@ -1,10 +1,8 @@
 public class pretest_m2 {
     public static void main(String[] args) {
-        
         String nama = "Gionaldo Chandrawansah";
         String nim = "105224031";
         String prodi = "Ilmu Komputer";
-        
         System.out.println("KARTU TANDA MAHASISWA\n" +
                            "NAMA\t: " + nama + "\n" +
                            "NIM\t: " + nim + "\n" +
